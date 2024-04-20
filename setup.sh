91ae10c24b7d7e7047c470122e20f4bbd35ba697
mattduerr <matt.duerr@pm.me>
2024-04-20 15:57:08 +0200

    Update script to unzip data in neo4j import dir

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 curl -o data.zip "https://opentransportdata.swiss/dataset/7cc000ea-0973-40c1-a557-7971a4939da3/resource/1cb3b923-6f2b-40ee-9a9a-900417e9fda3/download/gtfs_fp2024_2024-03-27_04-15.zip"
-unzip -d data data.zip
+sudo unzip -d /var/lib/neo4j/import/ data.zip
+# TODO: insert data in Neo4j from shell, with a single script for all constraint and insertions
