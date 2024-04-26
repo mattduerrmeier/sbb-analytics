6710461fad296aab35f42a5dfa308354c7cc3fe3
mattduerr <matt.duerr@pm.me>
2024-04-26 17:05:33 +0200

    Add script to drop neo4j db

---
+++ neo4j/drop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Please run the script as root."
+	exit
+fi
+
+read -r -p "This script will drop your neo4j database. Are you sure you want to proceed? [Y/n] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	echo "Stopping neo4j..."
+	systemctl stop neo4j
+	echo "Removing database files..."
+	rm -r /var/lib/neo4j/data/databases/neo4j 2>/dev/null
+	rm -r /var/lib/neo4j/data/transactions/neo4j 2>/dev/null
+	echo "Done! Please restart neo4j."
+else
+	echo "Cancelling script."
+	exit
+fi
