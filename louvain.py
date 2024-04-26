aed11945f5713549311e5a3d2ed0040c7de5d3fb
mattduerr <matt.duerr@pm.me>
2024-04-26 13:48:33 +0200

    Update modularity to use edge weights

--- louvain.py
+++ louvain.py
@@ -3,6 +3,7 @@ from typing import Any
 
 
 def louvain(G: nx.Graph) -> list[set[Any]]:
+    # TODO: fix m such that the value is the the original one
     m = len(G.edges())
     # 1: init each node as a community
     communities: list[set[Any]] = [{x} for x in sorted(G.nodes())]
@@ -77,8 +78,9 @@ def modularity_gain(
 ) -> float:
     d_ij = shared_degree(G, community_1, community_2)
 
-    d_i = sum([G.degree(n) for n in community_1])
-    d_j = sum([G.degree(n) for n in community_2])
+    # Use weights of the edge intead; if there is no weight use 1
+    d_i = sum([G.degree(n, data="weight", default=1) for n in community_1])
+    d_j = sum([G.degree(n, data="weight", default=1) for n in community_2])
 
     r = 1 / (2 * m)
     l = d_ij - (d_i * d_j) / m
@@ -153,4 +155,6 @@ comms_2 = louvain(new_graph)
 print(comms_2)
 new_graph_2 = hyper_nodes(G, comms_2)
 comms_3 = louvain(new_graph_2)
-print(comms_3)  # should return same community as comms_2, if we refer to the example given in the lecture
+print(
+    comms_3
+)  # should return same community as comms_2, if we refer to the example given in the lecture
