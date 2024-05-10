de555a0eacfef0e2aafc1930b9e948364718f1d3
olivia <olivialecomte33@gmail.com>
2024-05-10 16:42:18 +0200

    Split queries into two with locations first so that it doesn't get consumed. GeoJSON file is saved as stations.geojson and contains no duplicates.

--- create_edgelist.py
+++ create_edgelist.py
@@ -1,9 +1,11 @@
 from neo4j import GraphDatabase, Result
 import pandas as pd
 import sys
+import geopandas as gpd
+from shapely.geometry import Point
 
-# This script queries the neo4j database to create the sbb.edgelist.
-# You must provide the database username and passwords as arguments.
+# This script queries the neo4j database to create the sbb.edgelist and station locations GeoJSON.
+# You must provide the database username and password as arguments.
 
 if len(sys.argv) < 3:
     print("Please provide neo4j username and password as arguments")
@@ -18,43 +20,49 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
     info = driver.get_server_info()
     print(f"Connection established to {info.address}")
 
-    # cypher query
+    # cypher location query
     df = driver.execute_query(
         """
         MATCH (r:Route)<-[:USES]-(t:Trip)-[:STOPSAT]->(stt:StopTime)-[:REFERENCES]->(s:Stop) 
         WHERE r.type >= "100" AND r.type <= "120" 
-        WITH r, t, s, stt 
-        ORDER BY r.id, t.id, stt.stop_sequence 
-        RETURN r.id AS route_id, t.id AS trip_id, COLLECT({name: s.name, location: s.location}) AS stops 
+        WITH s.name AS station_name, collect(DISTINCT s.location)[0] AS lon_lat
+        ORDER BY station_name
+        RETURN station_name, lon_lat
         """,
         result_transformer_= Result.to_df,
     )
 
+# convert coordinates to Point objects
+df["geometry"] = df["lon_lat"].apply(lambda point: Point(point.x, point.y))
+df.drop(columns=["lon_lat"], inplace=True)
+
+# convert pandas df to geo df
+gdf = gpd.GeoDataFrame(df, geometry="geometry")
+
+# save geo df as geoJSON
+print("Writing station locations to stations.geojson")
+gdf.to_file("stations.geojson", driver='GeoJSON')
+
+
+# cypher query for edgelist
+df_edges = driver.execute_query(
+    """
+    MATCH (r:Route)<-[:USES]-(t:Trip)-[:STOPSAT]->(stt:StopTime)-[:REFERENCES]->(s:Stop) 
+    WHERE r.type >= "100" AND r.type <= "120" 
+    WITH r, t, s, stt 
+    ORDER BY r.id, t.id, stt.stop_sequence 
+    RETURN r.id AS route_id, t.id AS trip_id, COLLECT(s.name) AS paths 
+    """,
+    result_transformer_= Result.to_df,
+)
+
 # for each path list, create an edgelist (list of tuple)
-path_to_edgelist = lambda l: [(l[i]['name'], l[i + 1]['name']) for i in range(len(l) - 1)]
-df["paths"] = df["stops"].apply(path_to_edgelist)
+path_to_edgelist = lambda l: [(l[i], l[i + 1]) for i in range(len(l) - 1)]
+df_edges["paths"] = df_edges["paths"].apply(path_to_edgelist)
 
 # each tuple is not a single entry in the columns, instead of a list of tuple
-sbb_edgelist = df["paths"].explode().unique()
+sbb_edgelist = df_edges["paths"].explode().unique()
 
 print("Writing edgelist to sbb.edgelist")
 train = pd.DataFrame(list(sbb_edgelist), columns=["from", "to"])
 train.to_csv("sbb.edgelist", sep=";", header=False, index=False)
-
-# initialise empty dictionary
-station_data = {"station_name": [], "location": []}
-
-# append unique station names with respective location
-for stop_list in df["stops"].explode():
-    station_name = stop_list["name"]
-    location = stop_list["location"]
-    if station_name not in station_data["station_name"]:
-        station_data["station_name"].append(station_name)
-        station_data["location"].append(location)
-
-# create df
-station_locations = pd.DataFrame(station_data)
-
-# write to csv
-print("Writing station locations to station_locations.csv")
-station_locations.to_csv("station_locations.csv", sep=";", index=False)
--- setup_windows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Downloading dataset..."
-curl -o data.zip "https://opentransportdata.swiss/dataset/7cc000ea-0973-40c1-a557-7971a4939da3/resource/1cb3b923-6f2b-40ee-9a9a-900417e9fda3/download/gtfs_fp2024_2024-03-27_04-15.zip"
-sudo unzip -d /var/lib/neo4j/import/ data.zip
-
-echo "Starting neo4j insertion query. This may take a while... (~15 minutes)"
-cypher-shell -f neo4j/setup.cypher
