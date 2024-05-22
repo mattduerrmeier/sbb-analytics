d9b623f7b21bcdcf788b55a1ba4194cfe03b473e
christian <galley.christian@gmail.com>
2024-05-22 13:30:38 +0200

    fixing louvain, adding comments

--- louvain.py
+++ louvain.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import time
 from typing import TypeVar
-import copy
 
 T = TypeVar("T")
 
@@ -17,24 +16,23 @@ def louvain_implementation(G: nx.Graph) -> list[set[T]]:
 
 
 def louvain_generator(G: nx.Graph) -> list[set[T]]:
+    threshold = 0.001
     G.add_weighted_edges_from(G.edges(data="weight", default=1))
     m = len(G.edges())
     # 1: init each node as a community
     communities: list[set[T]] = [{v} for v in G.nodes()]
-
     evolved = True
     while evolved:
-        # TODO: check if we need a deepcopy
         original_communities = communities.copy()
         for v in G.nodes():
             # 2: remove the node from its community
-            communities = remove_v(communities, v)
+            communities, temp_comp = remove_v(communities, v)
 
             neighbors_communities = get_neighbors_communities(
                 G.neighbors(v), communities
             )
             # 3: add node to the community that maximizes delta
-            highest_delta_community = max_delta(G, v, neighbors_communities, m)
+            highest_delta_community, gain = max_delta(G, v, neighbors_communities, m)
             highest_delta_community.add(v)
 
         # 4: stop if process converges
@@ -44,31 +42,39 @@ def louvain_generator(G: nx.Graph) -> list[set[T]]:
     yield communities
 
     node2com = {n: n for n in G.nodes}
+    list_of_modularity = [threshold]
     # 5: create hypergraph
-    evolved = True
-    while evolved:
+
+    while max(list_of_modularity) >= threshold:
+        list_of_modularity = []
+
         G_hyper, node2com = hyper_graph(G, communities, node2com)
         communities = [{v} for v in G_hyper.nodes()]
-        original_communities = copy.deepcopy(communities)
-        for v in G_hyper.nodes():
-            # 2: remove the node from its community
-            communities = remove_v(communities, v)
-            neighbors_communities = get_neighbors_communities(
-                G_hyper.neighbors(v), communities
-            )
-            # 3: add node to the community that maximizes delta
-            highest_delta_community = max_delta(G_hyper, v, neighbors_communities, m)
-            highest_delta_community.add(v)
+        evolved = True
+        while evolved:
+            original_communities = communities.copy()
+            for v in G_hyper.nodes():
+                # 2: remove the node from its community
+                communities, temp_com = remove_v(communities, v)
+                neighbors_communities = get_neighbors_communities(
+                    G_hyper.neighbors(v), communities
+                )
+                # 3: add node to the community that maximizes delta
+                highest_delta_community, gain = max_delta(G_hyper, v, neighbors_communities, m)
+                highest_delta_community.add(v)
 
-        # stop if process converges or remaining 2 communities
-        if original_communities == communities or len(communities) <= 180:
-            evolved = False
-        yield rebuild_communities(node2com, communities)
+                if highest_delta_community != temp_com:
+                    list_of_modularity.append(gain)
+            # stop if process converges or remaining 2 communities
+            if original_communities == communities:
+                evolved = False
+            yield rebuild_communities(node2com, communities)
 
     return rebuild_communities(node2com, communities)
 
 
 # TODO: Improve this function. How can we get the neighbors without the nested for loop?
+# returns all communities adjacent to the node
 def get_neighbors_communities(
     neighbors: list[T], communities: list[set[T]]
 ) -> list[set[T]]:
@@ -85,23 +91,24 @@ def get_neighbors_communities(
 
 
 # removes node v from its community
-def remove_v(communities: list[set[T]], v: T) -> list[set[T]]:
+def remove_v(communities: list[set[T]], v: T) -> tuple[list[set[T]], set[T]]:
+    temp_com = None
     for com in communities:
         if v in com:
+            temp_com = com.copy()
             com.remove(v)
             # if the set is empty we should remove it
             if len(com) == 0:
                 communities.remove(com)
             break
+    return communities, temp_com
 
-    return communities
-
-
-def max_delta(G: nx.Graph, v: T, neigh_communities: list[set[T]], m: int) -> set[T]:
+# keep the highest modularity gain
+def max_delta(G: nx.Graph, v: T, neigh_communities: list[set[T]], m: int) -> tuple[set[T], float]:
     deltas = [modularity_gain(G, v, community, m) for community in neigh_communities]
-    return neigh_communities[deltas.index(max(deltas))]
+    return neigh_communities[deltas.index(max(deltas))], max(deltas)
 
-
+# compute modularity gain between a node and a community
 def modularity_gain(G: nx.Graph, v: T, community: set[T], m: int) -> float:
     # creating the dict once is faster than accessing the degrees twice
     degrees = dict(G.degree(community | {v}, weight="weight"))
@@ -115,6 +122,7 @@ def modularity_gain(G: nx.Graph, v: T, community: set[T], m: int) -> float:
     return 1 / (2 * m) * (d_ij - (d_i * d_j) / m)
 
 
+# merging all nodes within a community into a hypernode.
 def hyper_graph(
     G: nx.Graph, communities: list[set[T]], node2com: dict[T, int]
 ) -> tuple[nx.Graph, dict[T, int]]:
@@ -133,13 +141,13 @@ def hyper_graph(
         new_G.add_edge(com_1, com_2, weight=wt["weight"] + temp_wt["weight"])
     return new_G, node2com
 
-
+# tells to which community a node belongs to
 def node_to_which_community(comm, elem):
     for id, com in enumerate(comm):
         if elem in com:
             return id
 
-
+# keep track of which community each node is associated with. (useful when dealing with hypergraph)
 def rebuild_communities(dict_of_node, commu):
     final_comm = []
     for _ in commu:
@@ -184,14 +192,15 @@ edgelist = [
     (11, 13),
 ]
 
-# G = nx.Graph(edgelist)
-# G = nx.read_edgelist("data/sbb.edgelist", delimiter=";", create_using=nx.Graph)
-# connected_comp = nx.connected_components(G)
-# max_connected_comp = max(connected_comp)
-# sub_G = G.subgraph(max_connected_comp)
-# G = nx.Graph(sub_G)
-#
-# final_communities = louvain_communites(G)
-# print("Generator: ", len(final_communities))
+G = nx.Graph(edgelist)
+G = nx.read_edgelist("data/sbb.edgelist", delimiter=";", create_using=nx.Graph)
+connected_comp = nx.connected_components(G)
+max_connected_comp = max(connected_comp)
+sub_G = G.subgraph(max_connected_comp)
+G = nx.Graph(sub_G)
+
+final_communities = louvain_implementation(G)
+print(len(final_communities), final_communities)
+#print("Generator: ", len(final_communities))
 # print(count)
 # #gen = louvain_generator(G)
