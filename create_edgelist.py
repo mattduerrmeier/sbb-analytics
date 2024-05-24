bba52beac351e25de1bbf5eeec2989f515632717
mattduerr <matt.duerr@pm.me>
2024-05-24 13:18:03 +0200

    Fix scope of db driver and run demo Girvan Newman

--- create_edgelist.py
+++ create_edgelist.py
@@ -32,36 +32,36 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
         result_transformer_= Result.to_df,
     )
 
-# convert coordinates to Point objects
-df["geometry"] = df["lon_lat"].apply(lambda point: Point(point.x, point.y))
-df.drop(columns=["lon_lat"], inplace=True)
+    # convert coordinates to Point objects
+    df["geometry"] = df["lon_lat"].apply(lambda point: Point(point.x, point.y))
+    df.drop(columns=["lon_lat"], inplace=True)
 
-# convert pandas df to geo df
-gdf = gpd.GeoDataFrame(df, geometry="geometry")
+    # convert pandas df to geo df
+    gdf = gpd.GeoDataFrame(df, geometry="geometry")
 
-# save geo df as geoJSON
-print("Writing station locations to stations.geojson")
-gdf.to_file("data/stations.geojson", driver='GeoJSON')
+    # save geo df as geoJSON
+    print("Writing station locations to stations.geojson")
+    gdf.to_file("data/stations.geojson", driver='GeoJSON')
 
-# cypher query for edgelist
-df_edges = driver.execute_query(
-    """
-    MATCH (r:Route)<-[:USES]-(t:Trip)-[:STOPSAT]->(stt:StopTime)-[:REFERENCES]->(s:Stop) 
-    WHERE r.type >= 100 AND r.type <= 120
-    WITH r, t, s, stt 
-    ORDER BY r.id, t.id, stt.stop_sequence 
-    RETURN r.id AS route_id, t.id AS trip_id, COLLECT(s.name) AS paths 
-    """,
-    result_transformer_= Result.to_df,
-)
+    # cypher query for edgelist
+    df_edges = driver.execute_query(
+        """
+        MATCH (r:Route)<-[:USES]-(t:Trip)-[:STOPSAT]->(stt:StopTime)-[:REFERENCES]->(s:Stop)
+        WHERE r.type >= 100 AND r.type <= 120
+        WITH r, t, s, stt
+        ORDER BY r.id, t.id, stt.stop_sequence
+        RETURN r.id AS route_id, t.id AS trip_id, COLLECT(s.name) AS paths
+        """,
+        result_transformer_= Result.to_df,
+    )
 
-# for each path list, create an edgelist (list of tuple)
-path_to_edgelist = lambda l: [(l[i], l[i + 1]) for i in range(len(l) - 1)]
-df_edges["paths"] = df_edges["paths"].apply(path_to_edgelist)
+    # for each path list, create an edgelist (list of tuple)
+    path_to_edgelist = lambda l: [(l[i], l[i + 1]) for i in range(len(l) - 1)]
+    df_edges["paths"] = df_edges["paths"].apply(path_to_edgelist)
 
-# each tuple is not a single entry in the columns, instead of a list of tuple
-sbb_edgelist = df_edges["paths"].explode().unique()
+    # each tuple is not a single entry in the columns, instead of a list of tuple
+    sbb_edgelist = df_edges["paths"].explode().unique()
 
-print("Writing edgelist to sbb.edgelist")
-train = pd.DataFrame(list(sbb_edgelist), columns=["from", "to"])
-train.to_csv("data/sbb.edgelist", sep=";", header=False, index=False)
+    print("Writing edgelist to sbb.edgelist")
+    train = pd.DataFrame(list(sbb_edgelist), columns=["from", "to"])
+    train.to_csv("data/sbb.edgelist", sep=";", header=False, index=False)
